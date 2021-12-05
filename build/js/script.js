36c3094e0e720e96e17f56e5910ca5b1e4869477
Mikhail Kolesnichenko <santa_dj@mail.ru>
2021-12-05 23:51:35 +0300

    фÐdone season tickets

--- build/js/script.js
+++ build/js/script.js
@@ -1,18 +1,43 @@
 'use strict';
 // Тут будет кот ж)
 
-// var pageHeader = document.querySelector('.page-header');
-// var headerToggle = document.querySelector('.page-header__toggle');
-//
-// pageHeader.classList.remove('page-header--nojs');
-//
-// headerToggle.addEventListener('click', function () {
-//   if (pageHeader.classList.contains('page-header--closed')) {
-//     pageHeader.classList.remove('page-header--closed');
-//     pageHeader.classList.add('page-header--opened');
-//   } else {
-//     pageHeader.classList.add('page-header--closed');
-//     pageHeader.classList.remove('page-header--opened');
-//   }
-// });
+function renderTabs() {
+  if (document.querySelector('.tabs')) {
+    const START_SLIDE = 1;
 
+    const tabsArray = document.querySelectorAll('.tabs__item');
+    const tabsWrapper = document.querySelector('.tabs__cards-list');
+    const line = document.querySelector('.tabs__line');
+
+    function renderLine(number = 0) {
+      const currentNumber = number > 0? (number + START_SLIDE): START_SLIDE;
+      line.style.width = `${(currentNumber / tabsArray.length) * 100}%`;
+    }
+
+
+    tabsArray.forEach((item, number)=>{
+
+      function onClickTabsChange(evt) {
+        const targetButton = evt.target;
+
+        if (!targetButton.classList.contains('tabs__item-button--active')) {
+          tabsWrapper.style.transform = `translateX(-${number * 100}%)`;
+          renderLine(number);
+
+          tabsArray.forEach((el)=> {
+            const button = el.querySelector('.tabs__item-button');
+            button.classList.remove('tabs__item-button--active');
+          });
+
+          targetButton.classList.add('tabs__item-button--active');
+        }
+      }
+
+      item.addEventListener('click', onClickTabsChange);
+    });
+
+    renderLine();
+  }
+}
+
+renderTabs();
